d787b7555e8b41a0ffd850903e08ec341e176079
Brian Abelson <brianabelson@gmail.com>
2013-12-18 16:38:31 -0500

    making this ready for the blog post

--- 01-dataset.py
+++ 01-dataset.py
@@ -4,56 +4,84 @@ from pprint import pprint
 from urlparse import urljoin
 import dataset
 
-
-# A list of missed connections in New York
+# The base url for craigslist in New York
 BASE_URL = 'http://newyork.craigslist.org/'
 
-database = dataset.connect('sqlite:///missed_connections.db')
-
-table = database['missed_connections']
-
 def scrape_missed_connections():
-    """ Scrape all the deputies from the list """
+    """ Scrape all the missed connections from a list """
     
-    # Download the list.
+    # Download the list of missed connections
+
+    # here were using requests, 
+    # a python library for accessing the web
+
+    # we add "mis/" to the url to tell requests
+    # to get the missed connections 
+    # on newyork.craigslist.org
+
     response = requests.get(BASE_URL + "mis/")
 
-    # Parse HTML
+    # parse HTML using Beautiful Soup
+    # this returns a `soup` object which
+    # gives us convenience methods for parsing html
+
     soup = BeautifulSoup(response.content)
 
-    # Get all the links to missed connections:
-    for missed_connection in soup.find_all('span', {'class':'pl'}):
+    # find all the posts in the page.
+
+    # here we're telling BeautifulSoup to get us every
+    # span tag that has a class that equals pl
+
+    # these tags might look something like this:
+    # <span class='pl'> {content} </span>
+
+    missed_connections = soup.find_all('span', {'class':'pl'})
+
+    # Get all the links to missed connection pages:
+    for missed_connection in missed_connections:
         
+        # for each span list, find the "a" tag which 
+        # represents the link to the missed connection page.
+
         link = missed_connection.find('a').attrs['href']
+        
+        # join this relative link with the 
+        # BASE_URL to create an absolute link
+
         url = urljoin(BASE_URL, link)
         
-        # Scrape each missed connection's page.
+        # pass this url to a function (defined below) to scrape 
+        # info about that missed connection
+
         scrape_missed_connection(url)
 
-
 def scrape_missed_connection(url):
-    """ Extract information from a deputy's page. """
+    """ Extract information from a missed connections's page. """
 
-    # Build the full URL for each deputy; download & parse it.
+    # retrieve the missed connection with requests
+
     response = requests.get(url)
 
-    # Parse HTML
+    # Parse the html of the missed connection post
+
     soup = BeautifulSoup(response.content)
 
     # Extract the actual contents of some HTML elements:
+
+    # here were using BeautifulSoup's `text` method for retrieving
+    # the plain text within each HTML element.
+
+    # see and example of what this page looks like here:
+
     data = {
         'source_url': url,
         'subject': soup.find('h2', {'class':'postingtitle'}).text.strip(),
         'body': soup.find('section', {'id':'postingbody'}).text.strip(),
-        'email': soup.find('a', {'class':'replylink'}).text.strip(),
         'datetime': soup.find('time').attrs['datetime']
     }
-    
-    # Print the url
-    print "fetching " + url 
-    
-    # insert the data into the database
-    table.upsert(data, ['source_url'])
+
+    # Upsert the data into our database 
+    db['posts'].upsert(data, ['source_url'])
 
 
 if __name__ == '__main__':
--- 02-caching.py
+++ 02-caching.py
@@ -2,9 +2,59 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 from urlparse import urljoin
+import dataset
+import os
+from hashlib import sha1
 
-# The base url for craigslist in New York
+# A list of missed connections in New York
 BASE_URL = 'http://newyork.craigslist.org/'
+
+# connect to our database
+db = dataset.connect('sqlite:///missed_connections.db')
+
+# a directory for caching file's we've already downloaded
+CACHE_DIR = os.path.join(os.path.dirname(__file__), 'cache')
+
+def url_to_filename(url):
+    """ Make a URL into a file name, using SHA1 hashes. """
+
+    # use a sha1 hash to convert the url into a unique filename
+    hash_file = sha1(url).hexdigest() + '.html'
+    return os.path.join(CACHE_DIR, hash_file)
+
+
+def store_local(url, content):
+    """ Save a local copy of the file. """
+
+    # If the cache directory does not exist, make one.
+    if not os.path.isdir(CACHE_DIR):
+        os.makedirs(CACHE_DIR)
+
+    # Save to disk.
+    local_path = url_to_filename(url)
+    with open(local_path, 'wb') as f:
+        f.write(content)
+
+
+def load_local(url):
+    """ Read a local copy of a URL. """
+    local_path = url_to_filename(url)
+    if not os.path.exists(local_path):
+        return None
+
+    with open(local_path, 'rb') as f:
+        return f.read()
+
+
+def get_content(url):
+    """ Wrap requests.get() """
+    content = load_local(url)
+    if content is None:
+        response = requests.get(url)
+        content = response.content
+        store_local(url, content)
+    return content
+
 
 def scrape_missed_connections():
     """ Scrape all the missed connections from a list """
@@ -79,9 +129,9 @@ def scrape_missed_connection(url):
         'datetime': soup.find('time').attrs['datetime']
     }
 
-    # Print it prettily. 
-    pprint(data)
+    # Upsert the data into our database 
+    db['posts'].upsert(data, ['source_url'])
+
 
 if __name__ == '__main__':
     scrape_missed_connections()
-
