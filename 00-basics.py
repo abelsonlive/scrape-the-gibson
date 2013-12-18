6337c698d554deba03d0c9530d69cc1099fb074c
Brian Abelson <brianabelson@gmail.com>
2013-12-17 20:36:16 -0500

    Update 00-basics.py

--- 00-basics.py
+++ 00-basics.py
@@ -9,7 +9,7 @@ BASE_URL = 'http://newyork.craigslist.org/'
 
 
 def scrape_missed_connections():
-    """ Scrape all the deputies from the list """
+    """ Scrape all the missed connections from a list """
     
     # Download the list.
     response = requests.get(BASE_URL + "mis/")
@@ -20,15 +20,18 @@ def scrape_missed_connections():
     # Get all the links to missed connections:
     for missed_connection in soup.find_all('span', {'class':'pl'}):
         
+        # extract link path
         link = missed_connection.find('a').attrs['href']
+        
+        # join with base url
         url = urljoin(BASE_URL, link)
         
-        # Scrape each missed connection's page.
+        # pass missed connection url to a function to scrape info about that missed connection below
         scrape_missed_connection(url)
 
 
 def scrape_missed_connection(url):
-    """ Extract information from a deputy's page. """
+    """ Extract information from a missed connections's page. """
 
     # Build the full URL for each deputy; download & parse it.
     response = requests.get(url)
